d71cba24ead16ccabb00400e389d0b5d2c41fd29
Conrado F. Moreno-Fernández <cfmorenofernandez@gmail.com>
2023-11-09 10:18:55 -0500

    Manejo de roles para CRUD en front

---
+++ AymaraAcademiaBackEnd/src/main/java/pe/edu/upc/aaw/aymaraacademiaapi/controllers/RoleController.java
@@ -0,0 +1,58 @@
+package pe.edu.upc.aaw.aymaraacademiaapi.controllers;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.aaw.aymaraacademiaapi.dtos.RoleDTO;
+import pe.edu.upc.aaw.aymaraacademiaapi.entities.Role;
+import pe.edu.upc.aaw.aymaraacademiaapi.serviceinterfaces.IRoleService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/role")
+public class RoleController {
+    @Autowired
+    private IRoleService myService;
+
+    // Add an item to table
+    @PostMapping
+    public void registrar(@RequestBody RoleDTO dto) {
+        ModelMapper m = new ModelMapper();
+        Role myItem = m.map(dto, Role.class);
+        myService.insert(myItem);
+    }
+
+    // Delete an item by ID on table
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable("id")Integer id){
+        myService.delete(id);
+    }
+
+    // Retrieve an items by ID from table
+    @GetMapping("/{id}")
+    public RoleDTO listarId(@PathVariable("id")Integer id){
+        ModelMapper m = new ModelMapper();
+        RoleDTO myItem = m.map(myService.listId(id), RoleDTO.class);
+        return myItem;
+    }
+
+    // Retrieve all items from table
+    @GetMapping
+    public List<RoleDTO> listar(){
+        return myService.list().stream().map(x -> {
+            ModelMapper m = new ModelMapper();
+            return m.map(x, RoleDTO.class);
+        }).collect(Collectors.toList());
+    }
+
+    // (Exclusive to controller) Modify values on table
+    @PutMapping
+    public void modificar(@RequestBody RoleDTO dto) {
+        ModelMapper m = new ModelMapper();
+        Role d = m.map(dto, Role.class);
+        myService.insert(d);
+    }
+}
