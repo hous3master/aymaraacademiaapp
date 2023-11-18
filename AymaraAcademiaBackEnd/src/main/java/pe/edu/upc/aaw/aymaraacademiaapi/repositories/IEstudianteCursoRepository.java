8864e5358ded7a0f5800bb41bccc445fbe57338b
CamilaNav <caminavinta14@gmail.com>
2023-11-18 13:35:50 -0500

    Delete files

--- AymaraAcademiaBackEnd/src/main/java/pe/edu/upc/aaw/aymaraacademiaapi/repositories/IEstudianteCursoRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.aaw.aymaraacademiaapi.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.upc.aaw.aymaraacademiaapi.entities.EstudianteCurso;
-
-@Repository
-public interface IEstudianteCursoRepository extends JpaRepository<EstudianteCurso, Integer> {
-}
